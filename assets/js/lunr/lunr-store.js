cf2c2c1690d8ca1728c012e21b5c1d389131881b
BillHertzing <BillHertzing@users.noreply.github.com>
2021-05-23 23:46:50 +0000

    deploy: 6e4466df4487d4ec28d590d1a1203e6067b50972

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,48 +2,84 @@ var store = [{
         "title": "Welcome to Bill's Blog!",
         "excerpt":"This is my new Blog site, hosted by GitHub Pages. I’ve never been satisfied with all the effort needed to setup a Blog on other hosting sites; GitHub Pages combined with the Jekyll static site generator seems like it will meet my needs with very simple administration overhead. We will...","categories": [],
         "tags": [],
-        "url": "/Welcome-To-Bills-Blog/",
+        "url": "/2021/2021-04-06-welcome-to-bills-blog",
         "teaser": null
       },{
         "title": "How I Setup This Github Pages Blog Site Part 01",
         "excerpt":"I have been trying for a few years to setup a web site I can use for blogging. But since there are a lot of neat features I’d like to include, every prior attempt has foundered on getting it all to hang together, along with the problems of version control...","categories": ["technical"],
         "tags": ["Jekyll","GitHubPages"],
-        "url": "/technical/how-i-setup-this-github-pages-blog-site-part-01/",
+        "url": "/technical/2021/2021-04-06-how-i-setup-this-github-pages-blog-site-part-01",
         "teaser": null
       },{
         "title": "Welcome To The Personal Section Of My Site",
         "excerpt":"I’ve long hoped to have a website that I can use to share information with my friends and family, without having to create content for each individual social media site. This is it! If you would like to have access to my posts and pictures about family and friends, direct...","categories": ["personal"],
-        "tags": ["Introduction","Personal"],
-        "url": "/personal/Welcome-to-the-Personal-section-of-my-site/",
+        "tags": ["Personal"],
+        "url": "/personal/2021/2021-04-17-welcome-to-the-personal-section-of-my-site",
         "teaser": null
       },{
         "title": "Why I Wanted A Blog",
         "excerpt":"As a place to share knowledge, tips and how-tos, mostly regarding development, operations and monitoring of computer systems. As a place to publish the reasoning that underlie my personal stance on sometimes controversial political issues. As a place to share pictures and stories about myself and my family to other...","categories": ["political"],
-        "tags": ["political"],
-        "url": "/political/Why-I-wanted-a-Blog/",
+        "tags": ["Political"],
+        "url": "/political/2021/2021-04-17-why-i-wanted-a-blog",
         "teaser": null
       },{
         "title": "How I Setup This Github Pages Blog Site Part 02",
         "excerpt":"Welcome to the second part of this series detailing how I setup this blog site hosted on GitHub Pages. If you have not yet seen the first post in the series How I setup this GitHub Pages Blog Part 01, you should probably give it a quick review, to become...","categories": ["technical"],
         "tags": ["Jekyll","GitHubPages"],
-        "url": "/technical/how-i-setup-this-github-pages-blog-site-part-02/",
+        "url": "/technical/2021/2021-04-17-how-i-setup-this-github-pages-blog-site-part-02",
         "teaser": null
       },{
-        "title": "Eulogy for molly the dog",
+        "title": "Eulogy For Molly The Dog",
         "excerpt":"I didn’t think that the very first post I’d make under this personal section would be in memory of Molly. She went from healthy to deathly in just three weeks, between the birthday of this site and before the release of V0.03. So here I am writing the first personal...","categories": ["personal"],
-        "tags": ["Molly","Personal"],
-        "url": "/personal/Eulogy-for-Molly-the-Dog/",
+        "tags": ["Personal","Molly"],
+        "url": "/personal/2021/2021-04-24-eulogy-for-molly-the-dog",
         "teaser": null
       },{
         "title": "How I Setup This Github Pages Blog Site Part 03",
         "excerpt":"Welcome to the third part of this series detailing how I setup this blog site hosted on GitHub Pages. If you have not yet seen the first two post in the series How I setup this GitHub Pages Blog Site Part 01 and How I setup this GitHub Pages Blog...","categories": ["technical"],
         "tags": ["Jekyll","GitHubPages"],
-        "url": "/technical/how-i-setup-this-github-pages-blog-site-part-03/",
+        "url": "/technical/2021/2021-04-24-how-i-setup-this-github-pages-blog-site-part-03",
         "teaser": null
       },{
         "title": "Word Template for letter to CPA HOA regarding sale of golf course",
         "excerpt":"A reformatting of Jerry D’s response into a Microsoft Word letter . Overview When I decided to create a political section of my blog, I had grandiose visions of writing essays on the big political issues of the day. But In Real Life, the first political issue to come up...","categories": ["political"],
         "tags": ["HOA","Community Preservation Association"],
-        "url": "/political/Word-Template-for-letter-to-CPA-HOA-regarding-sale-of-golf-course/",
+        "url": "/political/2021/2021-05-01-word-template-for-letter-to-cpa-hoa-regarding-sale-of-golf-course",
+        "teaser": null
+      },{
+        "title": "Hiking Emigration Canyon Miners Trail",
+        "excerpt":"Pictures and description of hiking Emigration Canyon Miners Trail near SLC, UT AllTrails link AllTrails Recording of the hike Pictures alltrails 3d AllTrailsInfo Artsy water Emigration Canyon view Glacier Lilies amongst scrub oak Intrepid hikers KM on the trail Mountains across Emigration Canyon Spring runoff Videos Your browser does not...","categories": ["personal"],
+        "tags": ["Personal","AllTrails","Hiking"],
+        "url": "/personal/2021/2021-05-07-hiking-emigration-canyon-miners-trail",
+        "teaser": null
+      },{
+        "title": "Scout Cave Trail St George Ut",
+        "excerpt":"Pictures and links for a day hike along the Scout Cave Trail in St. George, UT Overview Each hike can teach us something. Today’s hike reinforced a lesson learned many times: Strangers met on the mountains can be a wonderful gift if you will interact with them. Today’s strangers included...","categories": ["personal"],
+        "tags": ["Hiking \"Scout Cave\" \"St. George\""],
+        "url": "/personal/2021/2021-05-17-scout-cave-trail-st-george-ut",
+        "teaser": null
+      },{
+        "title": "Petrified Sand Dunes Loop Trail St George Ut",
+        "excerpt":"Pictures and links for a day hike along the Petrified Sand Dunes Loop in St. George, UT Overview Todays hike was my birthday hike! 4.3 miles about 4 hours. Weather was warm and dry without a cloud in the sky. The geology was amazing. Here there are three completely different...","categories": ["personal"],
+        "tags": ["Hiking \"Petrified Sand Dunes\" \"St. George\""],
+        "url": "/personal/2021/2021-05-20-petrified-sand-dunes-loop-trail-st-george-ut",
+        "teaser": null
+      },{
+        "title": "Bills Blog Pre-Release Checklist",
+        "excerpt":"Things about the release candidate to validate before starting the steps in Bill’s Blog Release Process Conventions &lt;Feat/Bug/Post-Branch&gt; is the name of the branch that is being evaluated for release. The branch being evaluated for release will be referred to as the Release Candidate in this document. Criteria and Evaluation...","categories": ["technical"],
+        "tags": ["Pre-Release Checklist","Process"],
+        "url": "/technical/2021/2021-05-22-bills-blog-pre-release-checklist",
+        "teaser": null
+      },{
+        "title": "Bill's Blog Release Process",
+        "excerpt":"Detailed steps to finalize a Feature/Bugfix/Post branch, merge it with main, and release a new version of the site. Assumptions This process document was written assuming the developer is using Visual Studio Code (VSC) with both GitLens and GitGraph plugins. Instructions in this document may refer to either interactions with...","categories": ["technical"],
+        "tags": ["Release Process","Process"],
+        "url": "/technical/2021/2021-05-22-bills-blog-release-process",
+        "teaser": null
+      },{
+        "title": "How I Setup This Github Pages Blog Site Part 04",
+        "excerpt":"Welcome to the fourth part of this series detailing how I setup this blog site hosted on GitHub Pages. If you have not yet seen the first posts in the series How I setup this GitHub Pages Blog Site Part 01, How I setup this GitHub Pages Blog Site Part...","categories": ["technical"],
+        "tags": ["Jekyll","GitHubPages"],
+        "url": "/technical/2021/2021-05-22-how-i-setup-this-github-pages-blog-site-part-04",
         "teaser": null
       }]
